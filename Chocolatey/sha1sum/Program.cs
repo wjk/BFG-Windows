8aa8a395c018b66b97aa8f49e1b21547cb2b7ec8
William Kent <wjk011@gmail.com>
2015-09-25 21:00:13 -0400

    Add sha1sum project
    This is a small tool that computes the SHA-1 checksums of files.
    I wrote this tool to assist in checksumming files that are
    downloaded using a Chocolatey install script.

---
+++ Chocolatey/sha1sum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace sha1sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hasher = SHA1.Create();
+            foreach (string filename in args)
+            {
+                try
+                {
+                    byte[] fileHash = null;
+                    using (Stream stream = File.OpenRead(filename))
+                    {
+                        fileHash = hasher.ComputeHash(stream);
+                    }
+
+                    StringBuilder builder = new StringBuilder();
+                    foreach (byte b in fileHash) builder.Append(b.ToString("x2"));
+                    Console.WriteLine($"{builder.ToString()} {filename}");
+                }
+                catch (FileNotFoundException)
+                {
+                    Console.Error.WriteLine($"{filename}: File not found. Ignoring");
+                }
+                catch (IOException)
+                {
+                    Console.Error.WriteLine($"{filename}: IOException. Ignoring this file.");
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine($"{filename}: {ex.GetType().Name} was thrown. Ignoring this file.");
+                }
+            }
+        }
+    }
+}
---
+++ Chocolatey/sha1sum/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SHA1 Checksum Utility")]
+[assembly: AssemblyCompany("William Kent")]
+[assembly: AssemblyProduct("SHA1 Checksum Utility")]
+[assembly: AssemblyCopyright("Copyright © 2015 William Kent")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("80cf14f4-48fe-4212-bb1f-ebc8244b54d2")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
