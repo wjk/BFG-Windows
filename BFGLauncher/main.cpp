e65ff65e7fbb145079f4cca629b07158ea65cd40
William Kent <wjk011@gmail.com>
2015-09-26 22:05:50 -0400

    Add BFGLauncher executable project

---
+++ BFGLauncher/main.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <tchar.h>
+#include <JavaLauncher.h>
+
+// Change this when upgrading bfg-repo-cleaner
+static CString JarName = _T("bfg-1.12.5.jar");
+
+int _tmain(int argc, const TCHAR *argv[])
+{
+	CJavaLauncher java;
+	bool success;
+
+	// Prefer Java 1.8, as it's newer. Fall back to 1.7 if necessary.
+	// Note: Java before 1.7 is not supported.
+	success = java.FindJava(_T("1.8"));
+	if (!success) {
+		success = java.FindJava(_T("1.7"));
+		if (!success) {
+			_ftprintf(stderr, _T("ERROR: Could not find Java (1.7 required, 1.8 preferred)\n"));
+			return -1;
+		}
+	}
+
+	CString MyPath;
+	
+	LPTSTR buffer = MyPath.GetBuffer(MAX_PATH);
+	DWORD errorCode = GetModuleFileName(NULL, buffer, MAX_PATH);
+	MyPath.ReleaseBuffer();
+
+	if (errorCode == 0) {
+		_ftprintf(stderr, _T("ERROR: GetModuleFileName() failed with code %d\n"), GetLastError());
+		return -1;
+	}
+
+	CString JarPath;
+	CString MyDir = MyPath.Left(MyPath.GetLength() - MyPath.ReverseFind('\\'));
+	JarPath.AppendFormat(_T("%S\\%S"), MyDir.GetString(), JarName.GetString());
+
+	CAtlArray<CString> programArgv;
+	for (int i = 1; i < argc; i++) {
+		CString arg(argv[i]);
+		programArgv.Add(arg);
+	}
+
+	java.SetProgramArguments(programArgv);
+
+	long pid;
+	success = java.LaunchJar(JarPath, false, pid);
+	if (!success) {
+		_ftprintf(stderr, _T("ERROR: Could not launch java.exe\n"));
+		return -1;
+	}
+
+	long exitCode;
+	success = java.Wait(pid, exitCode);
+	if (!success) {
+		_ftprintf(stderr, _T("ERROR: CJavaLauncher::Wait() returned false\n"));
+		return -1;
+	}
+
+	return exitCode;
+}
