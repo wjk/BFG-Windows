bbcdbf8cd0c7c08da5aa7fc6a28568c85003feb5
William Kent <wjk011@gmail.com>
2017-03-08 22:08:06 -0500

    Use HTTPS for IKVM download

--- BFGRepoCleaner.Package.IKVM/tools/chocolateyInstall.ps1
+++ BFGRepoCleaner.Package.IKVM/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-$url = 'http://www.frijters.net/ikvmbin-8.1.5717.0.zip'
+$url = 'https://www.frijters.net/ikvmbin-8.1.5717.0.zip'
 $location = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
 Install-ChocolateyZipPackage 'ikvm' $url $location -checksum '260D63DA158103F9C5B385CAFF61BD47E78FB824C29C37373639C9488F973FB1' -checksumType 'SHA256'
