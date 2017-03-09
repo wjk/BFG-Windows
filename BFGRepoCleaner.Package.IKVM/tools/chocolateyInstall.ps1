9b03319aba5ef9c41729e729e80007f2f0b735a1
William Kent <wjk011@gmail.com>
2017-03-08 22:05:15 -0500

    Use SHA256 as the checksum algorithm in the IKVM package

--- BFGRepoCleaner.Package.IKVM/tools/chocolateyInstall.ps1
+++ BFGRepoCleaner.Package.IKVM/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 $url = 'http://www.frijters.net/ikvmbin-8.1.5717.0.zip'
 $location = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
-Install-ChocolateyZipPackage 'ikvm' $url $location -checksum '50ebac19ddb25c6f0082b071a366bde04f0ff2a4' -checksumType 'SHA1'
+Install-ChocolateyZipPackage 'ikvm' $url $location -checksum '260D63DA158103F9C5B385CAFF61BD47E78FB824C29C37373639C9488F973FB1' -checksumType 'SHA256'
