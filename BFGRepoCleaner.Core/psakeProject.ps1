5e046d9db60dbfda4d3bebbd2de6711e066ff80e
William Kent <wjk@users.noreply.github.com>
2018-05-01 14:34:52 -0400

    Update to bfg-1.13.0

--- BFGRepoCleaner.Core/psakeProject.ps1
+++ BFGRepoCleaner.Core/psakeProject.ps1
@@ -16,15 +16,15 @@ Properties {
 
 Task default -Depends build
 Task build -Depends DownloadJar
-Task DownloadJar -Description "Downloads bfg-1.12.15.jar and saves it into the `$(OutDir)" -RequiredVariables OutDir {
-	$jarpath = "$($OutDir)\bfg-1.12.15.jar"
-	Perform "Downloading bfg-1.12.15.jar" {
+Task DownloadJar -Description "Downloads bfg-1.13.0.jar and saves it into the `$(OutDir)" -RequiredVariables OutDir {
+	$jarpath = "$($OutDir)\bfg-1.13.0.jar"
+	Perform "Downloading bfg-1.13.0.jar" {
 		if (-not [System.IO.File]::Exists($jarpath)) {
-			iwr "http://repo1.maven.org/maven2/com/madgag/bfg/1.12.15/bfg-1.12.15.jar" -OutFile $jarpath
+			iwr "http://repo1.maven.org/maven2/com/madgag/bfg/1.13.0/bfg-1.13.0.jar" -OutFile $jarpath
 		}
 	}
 }
 
 Task clean -Description "Removes all built products" -RequiredVariables OutDir {
-	rm -Force "$($OutDir)\bfg-1.12.15.jar"
+	rm -Force "$($OutDir)\bfg-1.13.0.jar"
 }
